1587b47baf3f0c44a5edf87d3f669246b6814ce3
Bhanwar Singh Rathore <2021pietcsbhanwar040@poornima.org>
2024-12-09 17:48:05 +0530

    final update

--- src/app/(main)/(auth)/layout.tsx
+++ src/app/(main)/(auth)/layout.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="min-h-screen flex items-center justify-center ">
+    <div className="min-h-screen flex items-center justify-center md:mt-40 ">
       {children}
     </div>
   )
